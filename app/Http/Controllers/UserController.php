87b98c0f6834d6b99803e835c5919e59fa6447df
liyanovita <liyanovitasari13@gmail.com>
2024-09-23 22:03:52 +0700

    Jobsheet 4 Praktikum 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,24 +10,68 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $user = UserModel::create( [
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);  
-        $user->username = 'manager12';
-            
-        $user->save();
-            
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // true
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request)
+    {
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'), // Tanpa tanda kutip di sekitar variabel
+            'level_id' => $request->level_id
+
+        ]);
+
+        return redirect('/user');
+    }
+    public function ubah($id) {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    
+    public function ubah_simpan($id, Request $request) 
+    {
+        $user = UserModel::find ($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password'); // Tanpa tanda kutip
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
 }
+    public function hapus($id) {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+}
+
+}
+        // tambah data user dengan Eloquent Model
+        // $user = UserModel::create( [
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager11',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);  
+        // $user->username = 'manager12';
+            
+        // $user->save();
+            
+        // $user->wasChanged(); // true
+        // $user->wasChanged('username'); // true
+        // $user->wasChanged(['username', 'level_id']); // true
+        // $user->wasChanged('nama'); // false
+        // dd($user->wasChanged(['nama', 'username'])); // true
 
         // $userCount = UserModel::where('level_id', 2)->count();
         //return view('user', ['userCount' => $userCount]); prak 2.3
@@ -44,4 +88,3 @@ class UserController extends Controller
         //coba akses mode UserModel
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
-
