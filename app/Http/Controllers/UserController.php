6f72741ccb9290350d7bc640493947ce661d6f43
liyanovita <liyanovitasari13@gmail.com>
2024-09-23 19:46:14 +0700

    Jobsheet 4 Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,23 @@ class UserController extends Controller
     public function index()
     {
         // tambah data user dengan Eloquent Model
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password'=> Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        $user = UserModel::create( [
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);  
+        $user->username = 'manager12';
+            
         $user->save();
-        return view('user', ['data' => $user]);
+            
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
+    }
+}
 
         // $userCount = UserModel::where('level_id', 2)->count();
         //return view('user', ['userCount' => $userCount]); prak 2.3
@@ -37,5 +44,4 @@ class UserController extends Controller
         //coba akses mode UserModel
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
-    }
-}
+
