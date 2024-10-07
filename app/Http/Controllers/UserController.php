71a79feb49ae12196928db8c0a5178627260d76c
liyanovita <liyanovitasari13@gmail.com>
2024-10-07 15:31:20 +0700

    Jobsheet 5 Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,6 +40,11 @@ public function list(Request $request)
     $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
     return DataTables::of($users)
         // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
         ->addIndexColumn()
